d0223cbc3049a9eed6dac186162bb0f00211d918
DavidSosa <davidgsosa1@gmail.com>
2025-04-21 16:56:50 -0700

    Added more data, finished regression, working on regression plots

--- scripts/regression.R
+++ scripts/regression.R
@@ -25,4 +25,44 @@ merged_data_female$income <- income_state_by_state$`Median Income Female`[match(
 merged_data_male$foreign_born <- foreign_born_percentage$`Foreign-Born`[match(merged_data_male$State, foreign_born_percentage$State)]
 merged_data_female$foreign_born <- foreign_born_percentage$`Foreign-Born`[match(merged_data_female$State, foreign_born_percentage$State)]
 
-# demographics merging
+# race/demographics merging
+race_raw_data <- read_excel("Data.xlsx", sheet = "demographic of each state")
+
+demographic <- race_raw_data |>
+  select(State = Column1, White = Column2, Black = Column3, Hispanic = Column4, Asian = Column5) |>
+  filter(State != "United States") |>
+  mutate(across(where(is.character), as.numeric, .names = "num_{.col}"))
+
+new_data <- read.csv("cleaned_regression_data.csv")
+data <- read.csv("cleaned_regression_data.csv")
+
+
+data$Log_Male_Income <- log(data$Median_Income_Male)
+data$Log_Female_Income <- log(data$Median_Income_Female)
+
+model_male <- lm(Log_Male_Income ~ Average_Height +
+              Pct_Bachelors_or_more + White + Black + Hispanic + Asian +
+              Foreign.Born, data = data)
+
+
+model_female <- lm(Log_Female_Income ~ Average_Height  +
+                   Pct_Bachelors_or_more + White + Black + Hispanic + Asian +
+                   Foreign.Born, data = data)
+summary(model_male)
+summary(model_female)
+
+
+
+data$White <- scale(data$White, scale = FALSE)
+
+######################
+setwd("/Users/davidsosa/Code/school/regression-analysis/projects/height-vs-gdp/data/new_data")
+# move data
+data <- read.csv("combined_gender_income_data.csv")
+
+# Make Gender a factor
+data$Gender <- as.factor(data$Gender)
+
+# Run model with Gender as a variable
+model <- lm(Log_Income ~ Gender + Average_Height + Pct_Bachelors_or_more + White + Black + Hispanic + Asian + Foreign.Born, data = data)
+summary(model)
